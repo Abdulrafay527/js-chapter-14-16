4951f40f5ac68d779af892c96a69be1a3d1f7a37
Abdulrafay527 <rafaymoosait@gmail.com>
2025-07-29 16:30:27 +0500

    js-chapter-14,16 worked

---
+++ app.js
@@ -0,0 +1,55 @@
+
+// var studentNames = [];   
+
+
+// var studentNames = new Array();
+
+
+// var furits = ["apple" , "banana" , "mango"];
+
+// console.log(furits)
+
+
+// var flags = [true, false, true];
+
+// console.log(flags);
+
+
+// let mixedArray = [42, "text", false, 5.5];
+// console.log(mixedArray)
+
+
+// // Declare and initialize the array
+// var qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+// // Display the qualifications in the console
+// console.log("Educational Qualifications in Pakistan:");
+// for (var i = 0; i < qualifications.length; i++) {
+//     console.log((i + 1) + ") " + qualifications[i]);
+// }
+
+// var studentName = ["Michael" , "jhon" ,"tony"];
+// var score = [320 , 230 , 480 ];
+// var percentage ;
+// var userName = prompt("Enter your name");
+// // var scpore = prompt("Enter you/r score")
+// var totalMark = prompt("Define your total marks");
+// if(userName == studentName[0]){
+// percentage = score[0] / totalMark * 100
+// alert(percentage)
+// percentage = "";
+// }   
+// else if(userName == studentName[1]){
+// percentage = score[1] / totalMark * 100
+// alert(percentage)
+// percentage = "";
+// }
+// else if(userName == studentName[2]){
+//     percentage = score[2] / totalMark * 100
+//     alert(percentage)
+// percentage = "";
+
+// }
+// else{
+// alert("not enrolled");
+// };
